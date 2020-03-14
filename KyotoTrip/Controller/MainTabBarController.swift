2f18aa1cb70d81face8c7653f781080c7b27fb72
Masayuki Tanaka <masayuki5160@gmail.com>
2020-03-14 09:46:23 +0900

    Change tab bar and navigation bar color to white

---
+++ KyotoTrip/Controller/MainTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBarController.swift
+//  KyotoTrip
+//
+//  Created by TANAKA MASAYUKI on 2020/03/14.
+//  Copyright © 2020 TANAKA MASAYUKI. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBar.barTintColor = .white
+    }
+}
