4cc1398671351d5020bbef1d3defda08231e025f
Masayuki Tanaka <masayuki5160@gmail.com>
2020-03-20 18:46:23 +0900

    Add BusStopButton for MapView

--- KyotoTrip/Controller/MapViewController.swift
+++ KyotoTrip/Controller/MapViewController.swift
@@ -8,24 +8,33 @@
 
 import UIKit
 import Mapbox
+import RxSwift
+import RxCocoa
 
 class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MGLMapView!
+    @IBOutlet weak var busStopButton: UIButton!
     
     let kyotoStationLat = 34.9857083
     let kyotoStationLong = 135.7560416
     let defaultZoomLv = 13.0
-    let mbxStyleURL = "mapbox://styles/masayuki5160/ck7zskhvl0z781invsfh0fv46"
+    
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationItem.title = "NavigationBarTitleMap".localized
         
-        let url = URL(string: mbxStyleURL)
-        let mapView = MGLMapView(frame: view.bounds, styleURL: url)
-        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        // TODO: MapViewとbusボタンをVMに移行する方がいいのかも
         mapView.setCenter(CLLocationCoordinate2D(latitude: kyotoStationLat, longitude: kyotoStationLong), zoomLevel: defaultZoomLv, animated: false)
-        view.addSubview(mapView)
+        
+        busStopButton.layer.cornerRadius = 10.0
+        busStopButton.rx.tap.subscribe { (onNext) in
+            print("taped")
+            // TODO: ここでバス停の表示ON/OFF + バスルートのON/OFF
+        }.disposed(by: disposeBag)
     }
     
 }
