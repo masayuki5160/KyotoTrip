bc67c85634db83973f47f7048101588705d9e941
Masayuki Tanaka <masayuki5160@gmail.com>
2020-04-05 19:15:12 +0900

    Change async translate process to sync translate process

--- KyotoTrip/Modules/KyotoInfoTop/UseCase/KyotoCityInfoUseCase.swift
+++ KyotoTrip/Modules/KyotoInfoTop/UseCase/KyotoCityInfoUseCase.swift
@@ -11,6 +11,7 @@ import RxSwift
 import RxCocoa
 import SwiftyXMLParser
 import Alamofire
+import Firebase
 
 protocol KyotoCityInfoUseCaseProtocol: AnyObject {
     // note: UseCaseがRxに依存するのはいいのか? -> 厳密にはよくないが外部ライブラリに依存することを許容する
@@ -32,24 +33,34 @@ final class KyotoCityInfoUseCase: KyotoCityInfoUseCaseProtocol {
 
     func fetch() {
         // TODO: Kyotoというワードがなんども出てきて不要なので変数名などはあとで修正
-        kyotoCityInfoGateway.fetch { (response) in
+        kyotoCityInfoGateway.fetch {[weak self] (response) in
+            guard let self = self else { return }
+            
             switch response {
             case .failure(let error):
                 self.modelListPublishRelay.accept([])// TODO: Fix later
             case .success(let data):
-                // TODO: 翻訳処理の実装はあとで見直し
-                let translator = TranslatorGateway()
-                let translatedData = data.map { element -> KyotoCityInfo in
-                    var newElement = element
-                    translator.translate(source: element.title, targetLanguage: .en) { (translatedText) in
-                        newElement.title = translatedText
-                    }
-                    return newElement
-                }
-                // outputとして通知
-                self.modelListPublishRelay.accept(translatedData)
+                self.modelListPublishRelay.accept(data)
+                self.fetchTranslatedTextSync(source: data)
             }
         }
-        
     }
+    
+    private func fetchTranslatedTextSync(source list: [KyotoCityInfo]) {
+        // TODO: 翻訳処理が不要な場合は処理しない判定処理追加
+        let semaphore = DispatchSemaphore(value: 0)
+        let translatedList = list.map { element -> KyotoCityInfo in
+            var newElement = element
+            let translator = TranslatorGateway()
+            translator.translate(source: element.title, targetLanguage: .en) { (translatedText) in
+                newElement.title = translatedText
+                semaphore.signal()// 翻訳処理が完了したことを
+            }
+            semaphore.wait()// 翻訳処理が完了するまでwait
+            return newElement
+        }
+        // 翻訳が完了したことを通知
+        self.modelListPublishRelay.accept(translatedList)
+    }
+
 }
