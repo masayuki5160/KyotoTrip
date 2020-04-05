65cc799595288708019f84d40bf7c13f31376473
Masayuki Tanaka <masayuki5160@gmail.com>
2020-04-05 15:30:21 +0900

    Add KyotoCityInfoGateway

--- KyotoTrip/Modules/KyotoInfoTop/UseCase/KyotoCityInfoUseCase.swift
+++ KyotoTrip/Modules/KyotoInfoTop/UseCase/KyotoCityInfoUseCase.swift
@@ -13,51 +13,34 @@ import SwiftyXMLParser
 import Alamofire
 
 protocol KyotoCityInfoUseCaseProtocol: AnyObject {
-    func fetch()
-
-    // note: UseCaseがRxに依存するのはいいのか？？？ -> 厳密にはよくないが外部ライブラリに依存することを許容する
+    // note: UseCaseがRxに依存するのはいいのか? -> 厳密にはよくないが外部ライブラリに依存することを許容する
     var subscribableModelList: Observable<[KyotoCityInfo]> { get }
+    var kyotoCityInfoGateway: KyotoCityInfoGateway!{ get }
+
+    func fetch()
 }
 
 final class KyotoCityInfoUseCase: KyotoCityInfoUseCaseProtocol {
     
-    // TODO: InfoTopPageViewModelから移行した処理、あとで整理
-    private let rssUrlStr = "https://www.city.kyoto.lg.jp/menu2/rss/rss.xml"
     private let modelListPublishRelay = PublishRelay<[KyotoCityInfo]>()
-
     // TODO: KyotoCityInfoUseCaseProtocolにこのsubscribeを公開しておくことで良いのか？
     var subscribableModelList: Observable<[KyotoCityInfo]> {
         // TODO: mapで必要な処理を行いObservable or Driveを返す
         return modelListPublishRelay.asObservable()
     }
-    private var disposeBag = DisposeBag()
-    private var modelList: [KyotoCityInfo] = []
+    var kyotoCityInfoGateway: KyotoCityInfoGateway!
 
     func fetch() {
-        // TODO: 京都市のAPI経由でデータを集めてくる
         // TODO: Kyotoというワードがなんども出てきて不要なので変数名などはあとで修正
-        // let dummyInfo = [KyotoCityInfo()]
-        // self.output.useCaseDidFetchKyotoCityInfo(KyotoCityInfoList(kyotoCityInfoList: dummyInfo))
-        
-        // TODO: InfoTopPageViewModelから移行した処理、あとで整理
-        // TODO: この処理はUseCaseではなくGatewayという別のInterfaceAdapterレイヤーを挟んで処理すべき
-        Alamofire.request(rssUrlStr).responseData { [weak self] (response) in
-            guard let self = self else { return }
-            
-            if let data = response.data {
-                let xml = XML.parse(data)
-
-                for element in xml.rss.channel.item {
-                    var model = KyotoCityInfo()
-                    model.title = element.title.text ?? ""
-                    model.publishDate = element.pubDate.text ?? ""
-                    model.link = element.link.text ?? ""
-                    
-                    self.modelList.append(model)
-                }
-                
-                self.modelListPublishRelay.accept(self.modelList)
+        kyotoCityInfoGateway.fetch { (response) in
+            switch response {
+            case .failure(let error):
+                self.modelListPublishRelay.accept([])// TODO: Fix later
+            case .success(let data):
+                // outputとして通知
+                self.modelListPublishRelay.accept(data)
             }
         }
+        
     }
 }
