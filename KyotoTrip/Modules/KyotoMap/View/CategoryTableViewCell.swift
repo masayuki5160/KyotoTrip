cdce7749eed1aa32e42756244f25ad4919fa301f
Masayuki Tanaka <masayuki5160@gmail.com>
2020-04-30 16:53:46 +0900

    Add CategoryTableViewCell and bind to ViewController

---
+++ KyotoTrip/Modules/KyotoMap/View/CategoryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryTableViewCell.swift
+//  KyotoTrip
+//
+//  Created by TANAKA MASAYUKI on 2020/04/30.
+//  Copyright © 2020 TANAKA MASAYUKI. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
--- KyotoTrip/Modules/KyotoMap/View/CategoryViewController.swift
+++ KyotoTrip/Modules/KyotoMap/View/CategoryViewController.swift
@@ -11,6 +11,8 @@ import RxSwift
 import RxCocoa
 
 class CategoryViewController: UIViewController {
+    
+    // MARK: - Properties
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var testCategoryButton: UIButton!
@@ -21,19 +23,39 @@ class CategoryViewController: UIViewController {
     
     private var dependency: Dependency!
     private let disposeBag = DisposeBag()
-    
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setuUI()
+        bindPresenter()
+    }
+}
+
+// MARK: - Private functions
+
+private extension CategoryViewController {
+    private func setuUI() {
         tableView.register(UINib(nibName: "CategoryTableViewCell", bundle: nil), forCellReuseIdentifier: "CategoryTableViewCell")
-        
+    }
+    
+    private func bindPresenter() {
         dependency.presenter.bindCategoryButtonTapEvent(button: testCategoryButton.rx.tap.asObservable())
-        dependency.presenter.visibleFeatureDriver.drive(onNext: { [weak self] (features) in
-            print("test CategoryViewController")
-            print(features)
+
+        dependency.presenter.visibleFeatureDriver.drive(tableView.rx.items(cellIdentifier: "CategoryTableViewCell", cellType: CategoryTableViewCell.self)) { row, element, cell in
+            cell.title.text = element.title
+        }.disposed(by: disposeBag)
+
+        tableView.rx.modelSelected(VisibleFeature.self).subscribe(onNext: { feature in
+            // TODO: タップしたPOIをマップ上で示す
+            print("tapped cell \(feature.title)")
         }).disposed(by: disposeBag)
     }
 }
+
+// MARK: - Other extension
 
 extension CategoryViewController: DependencyInjectable {
     func inject(_ dependency: CategoryViewController.Dependency) {
