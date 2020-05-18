2d13968c06013c9d0bf6c786c44e12452bbde680
Masayuki Tanaka <masayuki5160@gmail.com>
2020-05-18 23:43:08 +0900

    Add Unit test for createVisibleFeature(category:coordinate:attributes:) in KyotoMapInteractor class

--- KyotoTripTests/KyotoMapInteractorTests.swift
+++ KyotoTripTests/KyotoMapInteractorTests.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 @testable import KyotoTrip
+import CoreLocation
 
 class KyotoMapInteractorTests: XCTestCase {
     
@@ -28,5 +29,33 @@ class KyotoMapInteractorTests: XCTestCase {
         let actual = interactor.updateUserPosition(current)
         
         XCTAssertEqual(expected, actual)
+    }
+    
+    func testCreateVisibleFeatureForBusstop() {
+        let mockAttributes = [
+            "path": "pathto/Documents/KyotoMapData/busstop/P11-10_26_GML/P11-10_26-jgd-g_BusStop.shp",
+            "P11_001": "五条千本",
+            "P11_003_1": "京都バス（株）,京都バス（株）",
+            "P11_002": "1,1",
+            "P11_004_1": "81,83",
+            "layer": "P11-10_26-jgd-g_BusStop"
+        ]
+        let mockCoordinate = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
+        
+        let actual = interactor.createVisibleFeature(
+            category: .Busstop,
+            coordinate: mockCoordinate,
+            attributes: mockAttributes
+        )
+        let expected = BusstopFeatureEntity(
+            title: mockAttributes["P11_001"]!,
+            subtitle: "",
+            coordinate: mockCoordinate,
+            type: .Busstop
+        )
+
+        XCTAssert(String(describing: type(of: expected)) == String(describing: type(of: actual)))
+        XCTAssertEqual(expected.title, actual.title)
+        XCTAssertEqual(expected.type, actual.type)
     }
 }
