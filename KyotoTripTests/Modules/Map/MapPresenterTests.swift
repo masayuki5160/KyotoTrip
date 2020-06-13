99229d91182be93686ef7830dfe04714a50e0c6b
Masayuki Tanaka <masayuki5160@gmail.com>
2020-06-13 23:17:24 +0900

    Add unit test for MapPresenter

--- KyotoTripTests/Modules/Map/MapPresenterTests.swift
+++ KyotoTripTests/Modules/Map/MapPresenterTests.swift
@@ -45,7 +45,7 @@ class MapPresenterTests: XCTestCase {
         //view.loadViewIfNeeded()
     }
     
-    func test_check_categoryButtonStatus() {
+    func test_check_categoryButtonStatus_whenRestaurantsIsVisible() {
         /// Input to Interactor
         interactor.didSelectBusstopButton(nextStatus: .visible)
         interactor.didSelectRestaurantButton(nextStatus: .visible)
@@ -63,6 +63,32 @@ class MapPresenterTests: XCTestCase {
         }).disposed(by: disposeBag)
 
         waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_check_categoryButtonStatus_whenRestaurantsIsHidden() {
+        /// Input to Interactor
+        interactor.didSelectBusstopButton(nextStatus: .visible)
+        interactor.didSelectRestaurantButton(nextStatus: .hidden)
+        interactor.didSelectCulturalPropertyButton(nextStatus: .hidden)
+        
+        let disposeBag = DisposeBag()
+        
+        let functionAnswered = expectation(description: "asynchronous function")
+        presenter.categoryButtonsStatusDriver.drive(onNext: { viewData in
+            XCTAssertEqual(CategoryButtonStatus.visible, viewData.busstop)
+            XCTAssertEqual(CategoryButtonStatus.hidden, viewData.restaurant)
+            XCTAssertEqual(CategoryButtonStatus.hidden, viewData.culturalProperty)
+
+            functionAnswered.fulfill()
+        }).disposed(by: disposeBag)
+
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_check_mapCenterPositionIsUserPosition_whenTapOneth() {
+    }
+    
+    func test_check_mapCenterPositionIsUserPosition_whenTapTwise() {
     }
     
     override func tearDown() {
