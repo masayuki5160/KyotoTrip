19b6d4924943d179ab28a1577952e8d5a783decb
Masayuki Tanaka <masayuki5160@gmail.com>
2020-06-21 10:20:51 +0900

    Add unit test for MapInteractor

--- KyotoTripTests/Modules/Map/MapInteractorTests.swift
+++ KyotoTripTests/Modules/Map/MapInteractorTests.swift
@@ -8,14 +8,55 @@
 
 import XCTest
 @testable import KyotoTrip
+import RxSwift
+import RxCocoa
 import CoreLocation
 
 class MapInteractorTests: XCTestCase {
     
     var interactor: MapInteractor?
+    var searchResults: RestaurantsSearchResultEntity?
 
     override func setUp() {
-        
+        let settings = RestaurantsRequestParamEntity()
+        searchResults = createRestaurantSearchResult()
+        let restaurantsSearchGatewayStub = RestaurantsSearchGatewayStub(result: .success(searchResults!))
+        let restaurantsRequestParamGateway = RestaurantsRequestParamGatewayStub(result: .success(settings))
+
+        interactor = MapInteractor(dependency: .init(
+            searchGateway: restaurantsSearchGatewayStub,
+            requestParamGateway: restaurantsRequestParamGateway,
+            languageSettingGateway: LanguageSettingGatewayStub(result: .success(.japanese)),
+            userInfoGateway: UserInfoGatewayStub(result: .success(true)))
+        )
+    }
+    
+    func test_check_isEmptyRestaurantMarker_whenRestaurantsButtonStatusIsHidden() {
+        interactor?.didSelectRestaurantButton(nextStatus: .hidden)
+        let disposeBag = DisposeBag()
+
+        let functionAnswered = expectation(description: "asynchronous function")
+        interactor?.restaurantMarkersDriver.drive(onNext: { markers in
+            XCTAssertEqual(0, markers.count)
+            functionAnswered.fulfill()
+        }).disposed(by: disposeBag)
+
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func test_check_isNotEmptyRestaurantMarker_whenRestaurantsButtonStatusIsVisible() {
+        interactor?.didSelectRestaurantButton(nextStatus: .visible)
+        let disposeBag = DisposeBag()
+
+        let functionAnswered = expectation(description: "asynchronous function")
+        interactor?.restaurantMarkersDriver.drive(onNext: { [weak self] markers in
+            let expectedCount = self?.searchResults?.rest.count
+            
+            XCTAssertEqual(expectedCount, markers.count)
+            functionAnswered.fulfill()
+        }).disposed(by: disposeBag)
+
+        waitForExpectations(timeout: 1, handler: nil)
     }
     
     private func createRestaurantSearchResult() -> RestaurantsSearchResultEntity {
