4af0d9c64b369ca8c10be8ab150f40f635711a25
Masayuki Tanaka <masayuki5160@gmail.com>
2020-06-27 00:31:38 +0900

    Remove unused file

--- KyotoTripTests/AppDefaultDependenciesStub.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AppDefaultDependenciesStub.swift
-//  KyotoTripTests
-//
-//  Created by TANAKA MASAYUKI on 2020/06/02.
-//  Copyright © 2020 TANAKA MASAYUKI. All rights reserved.
-//
-
-import Foundation
